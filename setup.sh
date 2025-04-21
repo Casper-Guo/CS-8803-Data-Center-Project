eeedc4dbbb9de1ea23a1a72c552234d2e161d51f
Casper Guo <casperg@umich.edu>
2025-04-21 18:24:05 -0400

    Install config command

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ sudo cp homa_prio cp_node metrics.py /usr/local/bin
 cd ..
 
 ./cloudlab/bin/config
+sudo cp ./cloudlab/bin/config ./cloudlab/bin/switch.py /usr/local/bin
 
 sudo apt-get update && sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-matplotlib
