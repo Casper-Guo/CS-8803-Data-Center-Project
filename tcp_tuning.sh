70e1886451229a4485c28f875f48369adf31ffdf
Casper Guo <casperg@umich.edu>
2025-05-27 18:49:05 -0500

    Bulk data (#2)
    * Setup
    
    * Make startup scripts executable
    
    * Updated scripts
    
    * updated ips
    
    * Updated scripts
    
    * Experiment profile and setup script
    
    * Follow HOMA repo node naming conventions
    
    * Stable profile
    
    * Updated rspec
    
    * custom cp_node files for server and client
    
    * 2 node profile and Python setup
    
    * Modified homa_setup.sh
    
    * say yes to prompt
    
    * Install some commands
    
    * Update README.md with SSH instructions
    
    * cp_basic initial
    
    * Change avg function to accept empty lists
    
    Change start_server signature to add the missing experiment name argument.
    
    This fix seemed to be fine yesterday, more validation needed.
    
    * Clean up repo
    
    * Auto confirm python install prompts and add README.md instructions
    
    * Added metric collection
    
    * Updated README.md for metrics
    
    * Update README.md
    
    * Update README.md
    
    * mixed workload
    
    * TCP mixed_workload
    
    * Enable default HOMA config
    
    * Install config command
    
    * TCP tuning settings
    
    ---------
    
    Co-authored-by: KunalDaga <46135668+KunalDaga@users.noreply.github.com>

---
+++ tcp_tuning.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo sysctl -w net.core.rmem_max=134217728
+sudo sysctl -w net.core.wmem_max=134217728
+sudo sysctl -w net.ipv4.tcp_rmem="4096 87380 67108864"
+sudo sysctl -w net.ipv4.tcp_wmem="4096 87380 67108864"
+sudo sysctl -w net.ipv4.tcp_window_scaling=1
